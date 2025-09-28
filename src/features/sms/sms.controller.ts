2fdb2f0a328140bb3d1514ffed4207d1e4c9bdf4
duckn1ght <duckn1ght04@gmail.com>
2025-09-28 11:08:58 +0500

    sms check fix

--- src/features/sms/sms.controller.ts
+++ src/features/sms/sms.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { SmsService } from './sms.service';
 import { CheckSmsDto } from './dto/check-sms.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UniversalJwtGuard } from '../auth/guards/universal.guard';
 
 @Controller('sms')
+@ApiTags('SMS')
+@ApiBearerAuth()
 export class SmsController {
   constructor(private readonly smsService: SmsService) {}
 
   @Post('check')
-  checkText(dto: CheckSmsDto) {
+  @UseGuards(UniversalJwtGuard)
+  checkText(@Body() dto: CheckSmsDto) {
     return this.smsService.checkText(dto);
   }
 }
