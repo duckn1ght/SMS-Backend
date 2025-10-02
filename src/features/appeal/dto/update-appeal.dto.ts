08234f30f8937cd576c860fbf5ffd1716eed21d8
Nurullaev Ilya <nurullaev567@gmail.com>
2025-10-02 13:06:31 +0500

    add response at appeal

--- src/features/appeal/dto/update-appeal.dto.ts
+++ src/features/appeal/dto/update-appeal.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAppealDto } from './create-appeal.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateAppealDto extends PartialType(CreateAppealDto) {}
+export class UpdateAppealDto extends PartialType(CreateAppealDto) {
+    @IsOptional()
+    @IsString()
+    response?: string;
+}
