a643564192fa824abf59cf58e32ee0c977d5b9ad
Nurullaev Ilya <nurullaev567@gmail.com>
2025-10-02 13:17:52 +0500

    Удалено поле response из UpdateAppealDto

--- src/features/appeal/dto/update-appeal.dto.ts
+++ src/features/appeal/dto/update-appeal.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAppealDto } from './create-appeal.dto';
-import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateAppealDto extends PartialType(CreateAppealDto) {
-    @IsOptional()
-    @IsString()
-    response?: string;
-}
+export class UpdateAppealDto extends PartialType(CreateAppealDto) {}
